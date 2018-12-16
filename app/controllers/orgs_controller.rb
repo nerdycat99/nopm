6a57f49365f49bbde59d1b297ff04485d4fc9595
nerdycat99 <info@drysky.co.uk>
2018-12-16 21:11:28 +0000

    with improved gui for sign up

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -18,8 +18,17 @@ class OrgsController < ApplicationController
   def create
     @thisorg = Org.create(org_params)
     # redirect_to new_user_registration_path(:orgid => @thisorg)  
-    redirect_to new_user_registration_path, flash: {org_name: @thisorg.name, org_id: @thisorg.id, user_type: 0}
-
+    # redirect_to new_user_registration_path(:org => @thisorg.id, :user_type => 0), flash: {org_name: @thisorg.name, org_id: @thisorg.id, user_type: 0}
+    # redirect_to new_user_registration_path(:org => @thisorg.id, :user => 0)
+    # alt approach - redirect to splash for steps 2
+    if @thisorg.invalid?
+      #render :new, status: :unprocessable_entity
+       # redirect_to register_space_path
+      # flash[:error] = '<strong>cannot create space</strong> you need to give your organisation or company a name.'
+    else
+      redirect_to register_user_path(:org => @thisorg.id, :user => 0)
+    end
+    
   end
   
 
--- app/models/org.rb
+++ app/models/org.rb
@@ -1,4 +1,5 @@
 class Org < ApplicationRecord
+  validates :name, presence: true
   has_many :users
   has_many :projects
 end
